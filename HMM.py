99d611b4c5c3c3f218f566d695486224745521d9
Christopher Brooks <cbrooks@usfca.edu>
2024-04-02 20:41:10 -0700

    Initial commit

---
+++ HMM.py
@@ -0,0 +1,65 @@
+
+
+import random
+import argparse
+import codecs
+import os
+import numpy
+
+# observations
+class Observation:
+    def __init__(self, stateseq, outputseq):
+        self.stateseq  = stateseq   # sequence of states
+        self.outputseq = outputseq  # sequence of outputs
+    def __str__(self):
+        return ' '.join(self.stateseq)+'\n'+' '.join(self.outputseq)+'\n'
+    def __repr__(self):
+        return self.__str__()
+    def __len__(self):
+        return len(self.outputseq)
+
+# hmm model
+class HMM:
+    def __init__(self, transitions={}, emissions={}):
+        """creates a model from transition and emission probabilities
+        e.g. {'happy': {'silent': '0.2', 'meow': '0.3', 'purr': '0.5'},
+              'grumpy': {'silent': '0.5', 'meow': '0.4', 'purr': '0.1'},
+              'hungry': {'silent': '0.2', 'meow': '0.6', 'purr': '0.2'}}"""
+
+
+
+        self.transitions = transitions
+        self.emissions = emissions
+
+    ## part 1 - you do this.
+    def load(self, basename):
+        """reads HMM structure from transition (basename.trans),
+        and emission (basename.emit) files,
+        as well as the probabilities."""
+        pass
+
+
+   ## you do this.
+    def generate(self, n):
+        """return an n-length observation by randomly sampling from this HMM."""
+        pass
+
+    def forward(self, observation):
+        pass
+    ## you do this: Implement the Viterbi algorithm. Given an Observation (a list of outputs or emissions)
+    ## determine the most likely sequence of states.
+    #allocate a matrix of s states and n observations.
+
+
+
+    def viterbi(self, observation):
+        pass
+        """given an observation,
+        find and return the state sequence that generated
+        the output sequence, using the Viterbi algorithm.
+        """
+
+
+
+
+
